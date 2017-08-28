f369f110a7c019089c4ce037c5730785378a2fdc
Debbie Michel and Kelsey Langlois <kelsey@>
2017-08-28 14:01:23 -0700

    Add UI to allow user to choose the shift amount and direction.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  console.log(encode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 15, "right"));
   $('#caesar-encode').click(function() {
     var input = $("#input").val();
     var output = encode(input, 3, "left");
@@ -12,5 +11,23 @@ $(document).ready(function(){
     var output = encode(input, 13, "left");
     $("#output").text(output);
     $("#input").val("");
+  });
+
+  $("#input-form").submit(function(event) {
+    event.preventDefault();
+    var message = $("#input").val();
+    var shift = parseInt($("#shift").val());
+    var direction = $("input:radio[name=direction]:checked").val();
+    if (shift > 0 && shift < 26) {
+      var output = encode(message, shift, direction);
+      $("#output").text(output);
+      $("#input").val("");
+      $("#shift").val("");
+      $("#right").prop("checked", false);
+      $("#left").prop("checked", true);
+    } else {
+      alert("Please enter a valid number to shift.");
+    }
+
   });
 });
