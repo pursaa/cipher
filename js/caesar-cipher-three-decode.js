02fef066c90261febfc4cf91855c3ff946403501
Guest User <Guest@epicodus-2.local>
2017-08-28 14:56:49 -0700

    create dynamic decode functions

--- js/caesar-cipher-three-decode.js
+++ js/caesar-cipher-three-decode.js
@@ -1,28 +1,53 @@
-function ceaserCipherThreeDecode(input){
+function caesarCipherThreeDecode(){
   event.preventDefault();
   var input = $("#input").val();
+  var dir = $('input[name="direction"]:checked').val();
+  var num = parseInt($("#num option:selected").val());
+  var alpha = 26;
   input = input.split("");
-  var x = 0;
-  for(var i = 0; i < input.length; i++){
-    x = input[i].charCodeAt();
-    if(x > 64 && x < 91){
-      if(x > 87){
-        x -= 23;
+  var temp = 0;
+  if(dir == "right"){
+    for(var i = 0; i < input.length; i++){
+        temp = input[i].charCodeAt();
+        if(temp > 64 && temp < 91){
+          temp += num;
+          if(temp > 90)
+            temp -= alpha;
+        }
+        if(temp > 96 && temp < 123){
+          temp += num;
+          if(temp > 122)
+            temp -= alpha;
+        }
+        console.log(temp);
+        input[i] = String.fromCharCode(temp);
       }
-      else{
-        x += 3;
+  } else if(dir == "left"){
+    i = input.length -1;
+    for(var j = 0; j < input.length; j++){
+      temp = input[i].charCodeAt();
+      console.log(temp);
+      if(temp > 64 && temp < 91){
+        temp -= num;
+        if(temp < 65)
+          temp+=alpha;
       }
+      if(temp > 96 && temp < 123){
+        temp -= num;
+        if(temp < 97)
+          temp += alpha;
+      }
+      input[i] = String.fromCharCode(temp);
+      i--;
     }
-    if(x > 96 && x < 123){
-      if(x > 119){
-        x -= 23;
-      }
-      else{
-        x += 3;
-      }
-    }
-    input[i] = String.fromCharCode(x);
   }
+
   input = input.join("");
   $("#output").text(input);
 }
+
+//
+// 1
+// x: = 90
+// y:
+// z:
