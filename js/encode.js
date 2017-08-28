1e8b1dee6f69f9830e89bf111bf7d4f28486846a
Debbie Michel and Kelsey Langlois <debbie@>
2017-08-28 09:50:06 -0700

    Update encode to encode single alphabetical characters, regardless of case.

--- js/encode.js
+++ js/encode.js
@@ -2,6 +2,12 @@ function encode(message) {
   if (/[^a-z]/i.test(message)) {
     return message;
   } else {
-    return "String was alpha";
+    var charCode = message.charCodeAt(0);
+    if (charCode >= 65 && charCode <= 67 || charCode >= 97 && charCode <= 99) {
+      charCode = charCode - 3 + 26;
+    } else {
+      charCode = charCode - 3;
+    }
+    return String.fromCharCode(charCode);
   }
 }
