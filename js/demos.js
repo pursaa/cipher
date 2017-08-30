3c159e644876f4266002bbd3a9c6106ae83bb76a
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-30 09:09:13 -0700

    Add updateable cipher alphabet to Caesar Cipher demo.

--- js/demos.js
+++ js/demos.js
@@ -4,6 +4,22 @@ function fillShiftSelect() {
   }
 }
 
+function fillAlphabet(cipherFunction, shift, direction) {
+  var result = "";
+  for (var i = 97; i < 123; i++) {
+    result += cipherFunction(String.fromCharCode(i), shift, direction) + " ";
+  }
+  return result;
+}
+
 $(document).ready(function() {
   fillShiftSelect();
+  $(".original-alphabet, .cipher-alphabet").text(fillAlphabet(encode, 0, "left"));
+
+  $("#demo-shift, input[name=demo-direction]").change(function() {
+    var shift = $("#demo-shift").val();
+    var direction = $("[name=demo-direction]:checked").val();
+    var output = fillAlphabet(encode, shift, direction);
+    $(".cipher-alphabet").text(output);
+  });
 });
