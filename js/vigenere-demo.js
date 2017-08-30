de177c90f15087d1b94286ddf49909c6993dac99
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-30 13:21:28 -0700

    Add function to add key to page, formatted correctly.

--- js/vigenere-demo.js
+++ js/vigenere-demo.js
@@ -11,11 +11,23 @@ function fillVigenereSquare() {
   return result;
 }
 
+function makeVigenereKey(key) {
+  var result = "";
+  for (var i = 0; i < key.length; i++) {
+    var char = key.charAt(i);
+    result += "<span class='key-" + i + "'>" + char + "</span>";
+  }
+  return result;
+}
+
 $(document).ready(function() {
   $(".original-message, .cipher-message").html(fillMessage(encode, 0, "left"));
   $(".vigenere-square").html(fillVigenereSquare());
 
   $("#vigenere-demo").submit(function(event) {
     event.preventDefault();
+    alert("got here");
+    var key = $("#vigenere-demo-key").val();
+    $(".vigenere-key").html(makeVigenereKey(key));
   });
 });
