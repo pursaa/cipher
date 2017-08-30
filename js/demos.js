1e8e5492ef812ccf24cc45b51e4be01b90dc78b9
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-30 09:41:23 -0700

    Add function to auto populate message fields.

--- js/demos.js
+++ js/demos.js
@@ -9,22 +9,36 @@ function fillShiftSelect() {
 function fillAlphabet(cipherFunction, shift, direction) {
   var result = "";
   for (var i = 97; i < 123; i++) {
-    result += cipherFunction(String.fromCharCode(i), shift, direction) + " ";
+    var code = cipherFunction(String.fromCharCode(i), shift, direction);
+    result += "<span class='letter-" + i + "'>" + code + " </span>";
   }
   return result;
 }
 
+function fillMessage(cipherFunction, shift, direction) {
+  var codeMessage = cipherFunction(testMessage, shift, direction);
+  var result = codeMessage.split("").map(function(character) {
+    var charCode = character.toLowerCase().charCodeAt(0);
+    return "<span class=letter-"+ charCode + ">" + character + "</span>";
+  });
+  return result.join("");
+}
+
+function encodeMessage() {
+
+}
+
 $(document).ready(function() {
   fillShiftSelect();
-  $(".original-alphabet, .cipher-alphabet").text(fillAlphabet(encode, 0, "left"));
-  $(".cipher-message").text(encode(testMessage, 0, "left"));
+  $(".original-alphabet, .cipher-alphabet").html(fillAlphabet(encode, 0, "left"));
+  $(".original-message, .cipher-message").html(fillMessage(encode, 0, "left"));
 
   $("#demo-shift, input[name=demo-direction]").change(function() {
     var shift = $("#demo-shift").val();
     var direction = $("[name=demo-direction]:checked").val();
     var cipherAlpha = fillAlphabet(encode, shift, direction);
-    var cipherMessage = encode(testMessage, shift, direction);
-    $(".cipher-alphabet").text(cipherAlpha);
-    $(".cipher-message").text(cipherMessage);
+    var cipherMessage = fillMessage(encode, shift, direction);
+    $(".cipher-alphabet").html(cipherAlpha);
+    $(".cipher-message").html(cipherMessage);
   });
 });
