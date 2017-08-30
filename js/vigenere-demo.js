839630cc8431c085c14c3a8a1e868dd99f7920c0
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-30 12:05:19 -0700

    Add function to make Vigenere Square.

--- js/vigenere-demo.js
+++ js/vigenere-demo.js
@@ -1,6 +1,18 @@
 var testMessage = "Hello, World!";
 
+function fillVigenereSquare() {
+  var result = "<p> &nbsp;&nbsp;" + fillAlphabet(encode, 0, "left") + "</p>";
+  for (var i = 97; i < 123; i++) {
+    var line = "<p>" + String.fromCharCode(i) + " " + fillAlphabet(encode, i - 97, "right") + "</p>";
+    result += line;
+  }
+  return result;
+}
+
 $(document).ready(function() {
+  $(".original-message, .cipher-message").html(fillMessage(encode, 0, "left"));
+  $(".vigenere-square").html(fillVigenereSquare());
+
   $("#vigenere-demo").submit(function(event) {
     event.preventDefault();
   });
