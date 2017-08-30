1a51e16f9e0a2eddaaee3a10cf1bdff92125d230
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-30 11:19:52 -0700

    Add comments to caesar-demo.js.

--- js/caesar-demo.js
+++ js/caesar-demo.js
@@ -1,11 +1,14 @@
+// Want a different example message? Change it here!
 var testMessage = "Hello world!";
 
+// Fills our select function with all shift values.
 function fillShiftSelect() {
   for (var i = 0; i < 26; i++) {
     $("#demo-shift").append("<option value='" + i + "'>" + i + "</option>")
   }
 }
 
+// Returns specified alphabet with each letter in labeled span.
 function fillAlphabet(cipherFunction, shift, direction) {
   var result = "";
   for (var i = 97; i < 123; i++) {
@@ -15,6 +18,7 @@ function fillAlphabet(cipherFunction, shift, direction) {
   return result;
 }
 
+// Returns specified ciphertext which each letter in labeled span.
 function fillMessage(cipherFunction, shift, direction) {
   var codeMessage = cipherFunction(testMessage, shift, direction);
   var result = codeMessage.split("").map(function(character) {
@@ -24,6 +28,7 @@ function fillMessage(cipherFunction, shift, direction) {
   return result.join("");
 }
 
+// Hightlights/darkens correct letter in each alphabet at a delay.
 function highlightAlphabet(charCode) {
   $(".original-alphabet, .cipher-alphabet").children().removeClass("highlighted");
   setTimeout(function() {
@@ -35,6 +40,7 @@ function highlightAlphabet(charCode) {
 
 }
 
+// Print next letter of ciphertext, highlighted, then darken when we move on.
 function displayCipher(cipherMessage, index) {
   var cipherArray = cipherMessage.split("</span>");
   setTimeout(function() {
@@ -46,23 +52,31 @@ function displayCipher(cipherMessage, index) {
   }, 1500);
 }
 
+// Handles all the messy business of animating the encoding.
 function highlightDisplay(cipherMessage, counter) {
+  // Don't want huge delay on animation start, do want delay on other letters.
   var delay;
   if (counter === 1) {
     delay = 500
   } else {
     delay = 2000;
   }
+  // Recursive setTimeout so that each letter shows one at a time.
   setTimeout(function() {
+    // Highlight correct letter in message.
     $(".original-message span:nth-child("+ (counter - 1)+ ")").removeClass("highlighted");
     var charCode = $(".original-message span:nth-child("+ counter + ")").attr("class");
     $(".original-message span:nth-child("+ counter + ")").addClass("highlighted");
+    // Highlight correct letter in alphabets.
     highlightAlphabet(charCode);
+    // Highlight and show correct letter in ciphertext.
     displayCipher(cipherMessage, counter - 1);
+    // Increment counter and check if we're at end of test message so we don't recurse forever.
     counter++;
     if (counter <= testMessage.length) {
       highlightDisplay(cipherMessage, counter);
     } else {
+      // We don't want to leave random highlighted letters at end.
       setTimeout(function() {
         $(".original-message span").removeClass("highlighted");
       }, 2000);
@@ -70,16 +84,19 @@ function highlightDisplay(cipherMessage, counter) {
   }, delay);
 }
 
+// Sets up start of highlightDisplay's recursion.
 function displayEncode(cipherMessage) {
   var counter = 1;
   highlightDisplay(cipherMessage, counter);
 }
 
 $(document).ready(function() {
+  // Sets up page, saving us lots of typing.
   fillShiftSelect();
   $(".original-alphabet, .cipher-alphabet").html(fillAlphabet(encode, 0, "left"));
   $(".original-message, .cipher-message").html(fillMessage(encode, 0, "left"));
 
+  // Any time our Caesar Cipher settings change, refresh our content.
   $("#demo-shift, input[name=demo-direction]").change(function() {
     var shift = $("#demo-shift").val();
     var direction = $("[name=demo-direction]:checked").val();
